40bf37437c3765f49f9d8e26ffce1f0e27e9c60d
HijjazD <154423459+HijjazD@users.noreply.github.com>
2026-04-28 21:48:15 +0800

    Update Hero.jsx

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = forwardRef((props, heroContainerRef) => {
         ? window.visualViewport.height
         : window.innerHeight;
 
-      document.documentElement.style.setProperty("--vh", `${vh*0.001}px`);
+      document.documentElement.style.setProperty("--vh", `${vh*0.01}px`);
     };
 
     setVH();
